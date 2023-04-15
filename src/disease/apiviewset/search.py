a1b0232b0e1524b0aa8637cef66d93cc372357e7
M Hadi Sasmito <keselyoleren@gmail.com>
2023-04-15 10:57:34 +0700

    search category

--- src/disease/apiviewset/search.py
+++ src/disease/apiviewset/search.py
@@ -1,3 +1,4 @@
+import contextlib
 from rest_framework import (
     status,
     generics,
@@ -6,7 +7,7 @@ from rest_framework import (
     views,
     response,
 )
-from disease.models.diseases_model import Disease
+from disease.models.diseases_model import Category, Disease
 from disease.models.news_model import News
 from disease.models.references_model import Article, Book, Report
 from disease.serializer.article import ArticleSerializer, GetArticleSerialize
@@ -17,47 +18,80 @@ from disease.serializer.report import GetReportSerialize, ReportSerializer
 from disease.views.book_views import BookListView
 from helper.choices import StatusChoice
 from helper.pagination import ResponsePagination
+from django.shortcuts import get_object_or_404
+
 
 class SearchApiView(generics.ListAPIView, viewsets.ModelViewSet, ResponsePagination):
     serializer_class = ReportSerializer
     
 
     def list(self, request, *args, **kwargs):
+        # sourcery skip: avoid-builtin-shadow
+        type = request.GET.get('type')
         category = request.GET.get('category')
         query = request.GET.get('keyword')
-        
         res = []
+
+        with contextlib.suppress(Exception):
+            category  = Category.objects.get(name=category)
         
-        if category == 'all':
+        if type == 'all':
             object_diseases = Disease.objects.filter(disease_name__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_diseases = Disease.objects.filter(disease_name__icontains=query, status=StatusChoice.APROVED, category=category)
             res.extend(DiseasesSerializer(object_diseases, many=True).data)
+        
             object_book = Book.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_book = Book.objects.filter(title__icontains=query, status=StatusChoice.APROVED, category=category)
             res.extend(BookSerializer(object_book, many=True).data)
+        
             object_artikel = Article.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_artikel = Article.objects.filter(title__icontains=query, status=StatusChoice.APROVED, category=category)
             res.extend(ArticleSerializer(object_artikel, many=True).data)
+        
             object_report = Report.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_report = Report.objects.filter(title__icontains=query, status=StatusChoice.APROVED, category=category)
             res.extend(ReportSerializer(object_report, many=True).data)
+        
             object_news = News.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_news = News.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
             res.extend(NewsSerializer(object_news, many=True).data)
 
-        if category == 'penyakit menular':
+        if type == 'penyakit menular':
             object_diseases = Disease.objects.filter(disease_name__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_diseases = Disease.objects.filter(disease_name__icontains=query, status=StatusChoice.APROVED, category=category)    
+
             res.extend(DiseasesSerializer(object_diseases, many=True).data)
 
-        if category == 'buku':
+        if type == 'buku':
             object_book = Book.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_book = Book.objects.filter(title__icontains=query, status=StatusChoice.APROVED, category=category)
             res.extend(BookSerializer(object_book, many=True).data)
 
-        if category == 'artikel':
+        if type == 'artikel':
             object_artikel = Article.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_artikel = Article.objects.filter(title__icontains=query, status=StatusChoice.APROVED, category=category)
             res.extend(ArticleSerializer(object_artikel, many=True).data)
 
-        if category == 'laporan':
+        if type == 'laporan':
             object_report = Report.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_report = Report.objects.filter(title__icontains=query, status=StatusChoice.APROVED, category=category)
+
             res.extend(ReportSerializer(object_report, many=True).data)
-        
-        if category == 'berita':
+
+        if type == 'berita':
             object_news = News.objects.filter(title__icontains=query, status=StatusChoice.APROVED)
+            if category:
+                object_news = News.objects.filter(title__icontains=query, status=StatusChoice.APROVED, category=category)            
+
             res.extend(NewsSerializer(object_news, many=True).data)
 
         paginated_data = self.paginate_no_queryset(res, request)
